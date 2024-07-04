46ced49ee932980b1ec4f11ad02b8f2ff3cdc74d
Rokis533 <rokiskicker@gmail.com>
2024-07-04 20:07:09 +0300

    sealed

---
+++ Access_Modifiers/Base.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    internal class Base
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine();
+        }
+        public virtual void Print2()
+        {
+            Console.WriteLine();
+        }
+    }
+    internal class Mid : Base
+    {
+        public sealed override void Print()
+        {
+            Console.WriteLine();
+        }
+        public override void Print2()
+        {
+            Console.WriteLine();
+        }
+    }
+    //internal class Low : Mid
+    //{
+    //    public override void Print()
+    //    {
+    //        Console.WriteLine();
+    //    }
+    //    public override void Print2()
+    //    {
+    //        Console.WriteLine();
+    //    }
+    //}
+}
