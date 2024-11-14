610384fbaaa4aeaea99ccc33c271b9be2c18f8f7
Rokis533 <rokiskicker@gmail.com>
2024-11-14 21:14:13 +0200

    http client

--- PotatoApi.Test/CalculatorControllerTests.cs
+++ PotatoApi.Test/CalculatorControllerTests.cs
@@ -1,18 +1,83 @@
 ﻿using AutoFixture.Xunit2;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Moq.Protected;
 using PotatoApi.Controllers;
 using PotatoApi.Entities;
+using PotatoApi.Test.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PotatoApi.Test
 {
     public class CalculatorControllerTests
     {
+
+
+        [Theory, PotatoData]              //Method_Scenario_Outcome
+        public async Task CalculateExternalApi_Response200_Successfull(Potato potato)
+        {
+            //Arange
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+
+            var httpclient = new HttpClient(httpMessageHandler.Object);
+            var sut = new CalculationService(httpclient);
+
+            httpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = (HttpStatusCode)200, //HttpStatusCode.OK
+                    Content = new StringContent(JsonSerializer.Serialize(potato))
+                });
+
+            //Act
+
+            var result = await sut.CalculateExternalApiAsync();
+
+            //Assert
+            Assert.NotNull(result);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         [Theory, AutoData]
         public void Calculate_NegativeMultiplication(int returnValue, Potato potato)
--- PotatoApi/CalculationService.cs
+++ PotatoApi/CalculationService.cs
@@ -6,10 +6,29 @@ namespace PotatoApi
     {
         int? Multiplier(int a, int b);
         int? Divide(int a, int b);
+        Task<string> CalculateExternalApiAsync();
     }
 
     public class CalculationService : ICalculationService
     {
+        private readonly HttpClient _httpClient;
+
+        public CalculationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> CalculateExternalApiAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Add("connectionId", "1KTIFTiiNKxbSddf-6CQiQ");
+            _httpClient.DefaultRequestHeaders.Add("X-API-KEY", "5B5UYJuCJCREFbQ7coTF0eyc8nC5Svbh5ZWqyUxmvfqEGWq9JsiF4EGAH4LmNiut");
+
+            var result = await _httpClient.GetAsync("https://atikas-space-exploration-api-abbng4buggadcxav.northeurope-01.azurewebsites.net/api/Authors");
+
+            result.EnsureSuccessStatusCode();
+
+            return await result.Content.ReadAsStringAsync();
+        }
 
         public int? Multiplier(int a, int b)
         {
